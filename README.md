2565c9192839ab98e3fbdb6758343ee2570f9fbe
sho <sskmy1024.y@gmail.com>
2019-07-17 14:38:40 +0900

    make some elements

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # 5W1HGameSite
 5W1Hゲームができるサイト
+
+## Require
+* docker
+* docker-compose
+
+
+## Try
+
+```
+docker-compose up -d
+
+# Open http://localhost:8000 in browser
+
+```
+
+
+# API
+
+`base_url` は適宜変えてください。
+> localで実行する場合は、`localhost:8000`
+
+### words
+
+#### Get random words
+  | method | endopoint          |
+  | :----: | ------------------ |
+  |  GET   | `base_url/api/all` |
+  
+ランダムで全ての要素のワードを返すエンドポイント
+
+* response body sample:
+  * success `200 OK`
+  ```json
+  [
+    {
+      "id": 2,
+      "word": "hogefuga"
+    }
+  ]
+  ```
+
+#### Get random word
+  | method | endopoint               |
+  | :----: | ----------------------- |
+  |  GET   | `base_url/api/:element` |
+  
+ランダムでワードを返すエンドポイント
+`:element` は、`all` `when` `where` `what` `why` `who` `how` を指定する。
+
+* response body sample:
+  * success `200 OK`
+  ```json
+  {
+    "id": 2,
+    "word": "hogefuga"
+  }
+  ```
--- backend/app.py
+++ backend/app.py
@@ -1,15 +1,16 @@
 # coding: utf-8
 
 from flask import Flask, request, make_response, redirect, abort
-from when import When
+from element import Element, When, Where, Why, What, Who, How
 import json
 
-When.initDatabase()
+Element.initAllDatabase()
 
 app = Flask(__name__)
 
+
 @app.route('/api/when', methods=['GET', 'POST'])
-def register():
+def registerWhen():
     if request.method == 'POST':
         params = request.get_json()
         result = When.register(params['word'])
@@ -18,6 +19,93 @@ def register():
         else:
             return json.dumps('409 Conflict'), 409
     elif request.method == 'GET':
-        return When.getRandom(), 200
+        word = When.getRandom()
+        if word:
+            return word.toJSON(), 200
+        else:
+            return "", 204
 
 
+@app.route('/api/where', methods=['GET', 'POST'])
+def registerWhere():
+    if request.method == 'POST':
+        params = request.get_json()
+        result = Where.register(params['word'])
+        if result:
+            return json.dumps(result.toJSON()), 201
+        else:
+            return json.dumps('409 Conflict'), 409
+    elif request.method == 'GET':
+        word = Where.getRandom()
+        if word:
+            return word.toJSON(), 200
+        else:
+            return "", 204
+
+
+@app.route('/api/who', methods=['GET', 'POST'])
+def registerWho():
+    if request.method == 'POST':
+        params = request.get_json()
+        result = Who.register(params['word'])
+        if result:
+            return json.dumps(result.toJSON()), 201
+        else:
+            return json.dumps('409 Conflict'), 409
+    elif request.method == 'GET':
+        word = Who.getRandom()
+        if word:
+            return word.toJSON(), 200
+        else:
+            return "", 204
+
+
+@app.route('/api/why', methods=['GET', 'POST'])
+def registerWhy():
+    if request.method == 'POST':
+        params = request.get_json()
+        result = Why.register(params['word'])
+        if result:
+            return json.dumps(result.toJSON()), 201
+        else:
+            return json.dumps('409 Conflict'), 409
+    elif request.method == 'GET':
+        word = Why.getRandom()
+        if word:
+            return word.toJSON(), 200
+        else:
+            return "", 204
+
+
+@app.route('/api/what', methods=['GET', 'POST'])
+def registerWhat():
+    if request.method == 'POST':
+        params = request.get_json()
+        result = What.register(params['word'])
+        if result:
+            return json.dumps(result.toJSON()), 201
+        else:
+            return json.dumps('409 Conflict'), 409
+    elif request.method == 'GET':
+        word = What.getRandom()
+        if word:
+            return word.toJSON(), 200
+        else:
+            return "", 204
+
+
+@app.route('/api/how', methods=['GET', 'POST'])
+def registerHow():
+    if request.method == 'POST':
+        params = request.get_json()
+        result = How.register(params['word'])
+        if result:
+            return json.dumps(result.toJSON()), 201
+        else:
+            return json.dumps('409 Conflict'), 409
+    elif request.method == 'GET':
+        word = How.getRandom()
+        if word:
+            return word.toJSON(), 200
+        else:
+            return "", 204
